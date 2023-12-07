f3607dfa9e3a27f911910812ec44e1d996a7ba53
André Moura <contato@andremourasantos.com>
2023-12-07 18:02:06 -0300

    added my first framework component and make it interactive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.com.br"
+  site: "https://www.com.br",
+  integrations: [vue()]
 });
---
+++ src/components/Greetings.vue
@@ -0,0 +1,39 @@
+<template>
+  <div @click="changeGreeting">
+    <p>{{greetingText}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const greetingText = ref<'Hi!' | 'Hello!'>('Hi!');
+
+    const changeGreeting = ():void => {
+      if(greetingText.value === 'Hi!'){
+        greetingText.value = 'Hello!';
+      } else {
+        greetingText.value = 'Hi!';
+      }
+    }
+
+    return {
+      greetingText,
+      changeGreeting
+    }
+  }
+})
+</script>
+
+<style scoped>
+  div {
+    background: white;
+    width: fit-content;
+    box-shadow: 0px 0px 10px #00000010;
+    border-radius: 16px;
+    padding: 16px;
+    cursor: pointer;
+  }
+</style>
